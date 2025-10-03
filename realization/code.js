f31c114099d4a39fe3ff9cfc8d65a284bef86066
ahmed <ahmedelmorabiti123460@gmail.com>
2025-10-03 11:04:01 +0100

    3.0

--- realization/code.js
+++ realization/code.js
@@ -6,5 +6,5 @@ function hello() {
 
     alert(`Thanks, ${name}! We received your message:\n${msg}\nWe'll contact you at ${email}${phone ? " or " + phone : ""}.`);
 
-    return false; // prevent actual form submission
+    return false;
 }
